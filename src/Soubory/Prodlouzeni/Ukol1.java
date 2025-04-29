d50c1ad9983a2f7ace10824f9480ab29a1707368
honza <bilekj@gasos-ro.cz>
2025-04-29 19:25:58 +0200

    r

--- src/Soubory/Prodlouzeni/Ukol1.java
+++ src/Soubory/Prodlouzeni/Ukol1.java
@@ -6,26 +6,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static java.nio.file.Files.newBufferedReader;
+import static java.nio.file.Files.newBufferedWriter;
+
 public class Ukol1 {
-    public static void main(String[] args) {
-        Path vstupniEmail = Path.of(System.getProperty("user.dir"), "vstupni_email.txt");
+    public static void main(String[] args) throws IOException {
+        Path vstup = Path.of("vstupni_email.txt");
+        Path vystup = Path.of("vystupni_email.txt");
 
+        BufferedReader reader = newBufferedReader(vstup);
+        BufferedWriter writer = newBufferedWriter(vystup);
+
+        reader.readLine();
+        String line;
         int pocetGmailu = 0;
-        try{
-            BufferedReader reader = Files.newBufferedReader(vstupniEmail);
-            BufferedWriter writer = Files.newBufferedWriter(vstupniEmail);
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.endsWith("@gmail.com")){
-                    writer.write(line);
-                    writer.newLine();
-                    pocetGmailu++;
-                }
-
-
+        while (reader.ready()) {
+            line = reader.readLine();
+            String[] gmailSplit = line.split("@");
+            String[] nameSplit = gmailSplit[0].split("\\.");
+            if (gmailSplit[1].equals("gmail.com")) {
+                writer.write(line+"\n");
+                pocetGmailu++;
             }
-        }catch (IOException e){
-            e.printStackTrace();
+            System.out.println(nameSplit[0]+" "+nameSplit[1]);
+            if (gmailSplit[1].equals("seznam.cz")) {
+                System.out.println(line+"\n");
+            }
         }
+        writer.close();
+        reader.close();
+        System.out.println("pocet gmailů: "+pocetGmailu);
     }
 }
